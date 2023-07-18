632072f4b13991a98e7733291bdcb593c5a4af18
aleksandrdrot <aleksandr_drot@rambler.ru>
2023-07-18 23:46:24 +0300

    DataBase SQL Avatar

---
+++ school/src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
@@ -0,0 +1,12 @@
+package ru.hogwarts.school.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.hogwarts.school.model.Avatar;
+
+
+
+
+public interface AvatarRepository extends JpaRepository<Avatar, Long> {
+
+    Avatar findAvatarByStudentId(Long Id);
+}
