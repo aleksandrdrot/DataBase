38c7ce77ccbe47000c2ec63ad97b4e907ebccf86
aleksandrdrot <aleksandr_drot@rambler.ru>
2023-07-25 21:15:43 +0300

    DataBase scripts SQL

--- school/scripts421.sql
+++ school/scripts421.sql
@@ -3,7 +3,9 @@ ALTER TABLE student ALTER COLUMN age SET DEFAULT 20;
 ALTER TABLE student ADD CONSTRAINT name_unique UNIQUE (name);
 ALTER TABLE student ALTER COLUMN name SET NOT NULL;
 
-ALTER TABLE faculty ADD CONSTRAINT name_faculty_unique UNIQUE (name);
-ALTER TABLE faculty ADD CONSTRAINT color_unique UNIQUE (color);
+CREATE UNIQUE INDEX faculty_index ON faculty (name, color)
+
+-- ALTER TABLE faculty ADD CONSTRAINT name_faculty_unique UNIQUE (name);
+-- ALTER TABLE faculty ADD CONSTRAINT color_unique UNIQUE (color);
 
 
